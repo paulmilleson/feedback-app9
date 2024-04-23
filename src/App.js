72eecc4ae4838f5a90096ad65270bfe8efcbcb12
milleson <paul_milleson@yahoo.com>
2024-04-23 15:39:18 -0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>My First React App</h1>
+      <p>Welcome to my React app!</p>
+    </>
+ 
   );
 }
+export default App;   
 
-export default App;
+// in JSX, only 1 root element can be returned, so we
+// could enclose all of our sub-elements in one parent
+// element (in this case a div set) - but a code 
+//fragment can also be used, (and won't show up in the
+// html) consisting of <></> !
+
+// If you want to use a class in a component, you can do so
+// by:
+// using the word: className instead of the word: class
+// (which is an illegal word in JSX.)
+
+// and if you want to use for= in an html tag, you can do so
+// by using the word htmlFor instead of the word for
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import React from "react";
+import ReactDOM from "react-dom"; 
+//library that allows us to render React components to the DOM
+import "./index.css";
+import App from "./App";
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>, 
+    document.getElementById("root")
+);   
+// render the App Component to the root element in the DOM
+// (the root element is in the public/index.html file)
+// the App component is the parent component of the entire
+// application.
+// the App component is imported from the App.js file.
+// the App component is a functional component that returns
+// JSX.
+// the App component is wrapped in a <React.StrictMode> tag.
+// the <React.StrictMode> tag is a tool for highlighting
+// potential problems in an application.
+// the <React.StrictMode> tag only affects development mode.
+// the <React.StrictMode> tag does not render any visible UI.
+// the <React.StrictMode> tag activates additional checks and
+// warnings for its descendants.
+// the <React.StrictMode> tag is a self-closing tag.
+// the <React.StrictMode> tag wraps the App component.
